23f5287426a9291ab89c002f64f400ad91636c6a
Adam Stacoviak <adam@stacoviak.com>
2011-09-27 19:20:43 -0500

    Bumped to 0.0.2

--- lib/zocial.rb
+++ lib/zocial.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("zocial", :path => "#{File.dirname(__FILE__)}/..")
 
 module Zocial
   
-  VERSION = "0.0.1"
-  DATE = "2011-09-24"
+  VERSION = "0.0.2"
+  DATE = "2011-09-27"
 
 end
